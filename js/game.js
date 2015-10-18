8433bb38fa394f321061d4934750817c6052e09d
DaisyCream@qq.com <DaisyCream@qq.com>
2015-10-18 10:50:52 +0800

    deal with the scoremove

--- js/game.js
+++ js/game.js
@@ -26,23 +26,34 @@ var GAMEING = function(){};
 var starFrame = document.getElementsByClassName('starFrame');
 GAMEING.score = 0;
 GAMEING.life = 3;
+GAMEING.everRandom = 0;
 $$('score').innerHTML = GAMEING.score;
 $$('life').innerHTML = GAMEING.life;
 GAMEING.showStar = function(){
     var startRandom = parseInt(Math.random()*starFrame.length);
+    while(startRandom==GAMEING.everRandom){
+        startRandom = parseInt(Math.random()*starFrame.length);
+    }
     var starTag = document.createElement('div');
     starTag.setAttribute('class','star');
     starFrame[startRandom].appendChild(starTag);
+    var scoreX = getElementLeft(starTag);
+    var scoreY = getElementTop(starTag);
+    console.log(scoreX);
+    console.log(scoreY);
+
     var isTouch = setTimeout(function(){
         GAMEING.passStar(starTag);
-        console.log('false');
     },1600);
+
     starTag.onmouseover = function(){
+        GAMEING.createScore(scoreX,scoreY);
         GAMEING.touchStar(starTag);
         clearTimeout(isTouch);
-        console.log(clearTimeout);
     };
-    setTimeout(GAMEING.showStar,1000);
+    GAMEING.everRandom = startRandom;
+
+    setTimeout(GAMEING.showStar,5000);
 };
 
 
@@ -57,6 +68,63 @@ GAMEING.passStar = function(target){
     target.parentNode.removeChild(target);
     GAMEING.life--;
     $$('life').innerHTML = GAMEING.life;
+};
+
+
+GAMEING.createScore = function(x,y){
+    var score = document.createElement('div');
+    score.setAttribute('class','scoreAnimation');
+    score.innerHTML = '+1';
+    score.style.left = x - 10 + 'px';
+    score.style.top = y - 10 + 'px';
+    document.body.appendChild(score);
+    console.log("create true");
+    setTimeout(function(){
+        GAMEING.scoreLocationChange(score);
+    },50);
+
+};
+
+GAMEING.scoreLocationChange = function(target){
+    target.style.top = '0px';
+    target.style.left = '0px';
+};
+
+/********************************getElement location**************************************/
+
+
+/***
+ * This is get the element's absolute top
+ * @param target
+ * @returns {Number|number}
+ */
+
+var getElementTop = function(target){
+    var top = target.offsetTop;
+    var current = target.offsetParent;
+    while(current!=null){
+        top += current.offsetTop;
+        current = current.offsetParent;
+    }
+    return top;
+};
+
+
+/***
+ * This is get the element's absolute left
+ * @param target
+ * @returns {Number|number}
+ */
+
+
+var getElementLeft = function(target){
+    var left = target.offsetLeft;
+    var current = target.offsetParent;
+    while(current!=null){
+        left += current.offsetLeft;
+        current = current.offsetParent;
+    }
+    return left;
 };
 
 window.onload = function(){
